ef9cb627e3a6991894e151eef1eb8c43c5b4555f
jrajni <jrajni6@gmail.com>
2018-11-23 18:37:38 +0530

    todo api in text file using node

--- fs_todo/server.js
+++ fs_todo/server.js
@@ -1,13 +1,18 @@
+//fs is variable in which fs module imports
 var fs = require("fs");
+//axios is a variable in which axios module imports
 var axios = require("axios")
+//url is a variable in which todo api stored
 var url= "https://jsonplaceholder.typicode.com/todos/1"
+//axios uses .get method to use url 
 axios.get(url)
+//.then method is used to get response 
 .then((response)=>{
-   // console.log(response)
-
-
+//todo api data is written in textfile using fs module with writeFile method
     fs.writeFile("input.text",JSON.stringify(response.data,null,2),(err)=>{
+//if there is error then throw        
         if (err) throw err;
+//print error
         console.log("file created");
     }) 
 })
