d84ea7c47a5210522cf41537b5c711fd683793c0
Arr Jay <jrajni6@gmail.com>
2019-02-06 14:48:25 +0530

    Update server.js

--- todo_api/server.js
+++ todo_api/server.js
@@ -2,7 +2,7 @@
 var axios = require ("axios");
 //url is variable which stores my todolist api
 var url= "https://jsonplaceholder.typicode.com/todos/1"
-//.get is used to feth inforamtion that client expect from server
+//.get is used to fetch inforamtion that client expect from server
 axios.get(url)
 //.then is promise method which helps to give response either by resolving or by rejecting and
 //give result of asnychronous 
